92bbb44e003df7b076ea04b9d504f8a1d69151f9
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-10-27 17:46:57 -0500

    Hola

---
+++ src/main/java/zamira/estudiante/vectores_matrices/Vectores_Matrices.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package zamira.estudiante.vectores_matrices;
+
+import java.util.Scanner;
+
+public class Vectores_Matrices {
+
+    public static void main(String[] args) {
+        
+        Scanner sandy=new Scanner(System.in);
+        
+        System.out.println("Ingresa el primer numero");
+        int num1=sandy.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        int num2=sandy.nextInt();
+        int suma=num1+num2;
+     
+        System.out.println("La suma es:"+" "+suma );
+        
+
+        //VECTORES  
+        //AREGLO DE MATRICES  new=sirve para reservar un espacio en memoria
+        /*
+     * UN ARREGLO DE UNA SOLA DIMENSION ES UN VERCTOR
+     * INDICE, POSICION VALOR
+      
+      declar/ declarando = (int edad)
+      inicialisando = (edad=0)*/
+        //declarando un vector
+      /*  int[] enteros;
+
+        //iniciando un vector
+        
+        enteros = new int[3];
+        //inicializar los valores de un vector
+        enteros[0] = 5;
+        enteros[1] = 7;
+        enteros[2] = 9;
+       /* for (int i = 0; i<=2; i=i+1) {
+            System.out.println("Los valores del vector son:" + " " + enteros[i]);
+            //+enteros[i]+"esta en la posicion"+i
+            //el valor i marca la posición
+        }*/
+      /* int j=enteros[1];
+        System.out.println("El valor de la posicion 1 es:"+j);
+        
+        int a=enteros[0];
+        System.out.println("El valor de la posicion 0 es:"+a);
+        
+        int d=enteros[2];
+        System.out.println("El valor de la posicion 2 es:"+d);*/
+      
+      
+/*
+      int [][] enteros;
+      enteros = new int[3][3];
+      
+      enteros[0][0]=33;
+      enteros[1][0]=45;
+      enteros[1][1]=23;
+      
+      for(int i=0;i<3;i=i+1){
+          System.out.println("los valores de la matriz:"+""+enteros[i][j]);
+          
+      }*/
+      
+      
+
+          
+    }
+}
